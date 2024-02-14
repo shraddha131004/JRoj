d0a0a964a538c48707669bcf594ace9b74a0bd91
shraddha131004 <2022bit036@sggs.ac.in>
2024-02-15 04:26:07 +0530

    daily code

---
+++ feb14.java
@@ -0,0 +1,13 @@
+
+class Father{
+	int age = 35;
+	static String name;
+	public Father(double age){
+	    //double x = age = age;
+	    	    System.out.println(age+0.9);
+	}
+	public static void main(String args[]){
+	    Father daughter = new Father(12.0);
+	    System.out.println(daughter.age); 
+	}
+}
