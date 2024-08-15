fb56ac12888930c004bdc456e5bc61c65a2f9577
shraddha131004 <155845084+shraddha131004@users.noreply.github.com>
2024-08-15 23:39:24 +0530

    Add files via upload

---
+++ sti.java
@@ -0,0 +1,10 @@
+class S {
+    public static void main(String[] args) {
+        String age = "10";
+        
+        int age_to_int = Integer.parseInt(age);
+        
+        System.out.println(age_to_int + 20);
+        // 30
+    }
+}
